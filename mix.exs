8ac34a2b0e901b6d772edf7e32cd7351aacd0ca6
Vitor Cavalcanti de Albuquerque <1920397+vrcca@users.noreply.github.com>
2020-10-22 20:27:51 +0200

    Deletes comments from mix.exs

--- mix.exs
+++ mix.exs
@@ -17,15 +17,6 @@ defmodule AWS.CodeGen.Mixfile do
     [extra_applications: [:logger, :eex]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     [{:earmark, "~> 1.4", only: :dev},
      {:ex_doc, "~> 0.21", only: :dev},
