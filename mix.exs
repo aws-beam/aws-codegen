be3480f3b1eb8a75ef73ad1f2ae20611a6711cd2
Vitor Cavalcanti de Albuquerque <1920397+vrcca@users.noreply.github.com>
2020-10-29 20:18:08 +0100

    Increases min Elixir version for the project

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AWS.CodeGen.Mixfile do
   def project do
     [app: :aws_codegen,
      version: "0.0.1",
-     elixir: "~> 1.4",
+     elixir: "~> 1.8",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
