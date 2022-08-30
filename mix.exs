d2db6d0b31a7fdd1845b6e4694c5b8cedb3ba867
Philip Sampaio <philip.sampaio@gmail.com>
2022-08-30 16:10:19 +0200

    Update poison to v5.0.0

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule AWS.CodeGen.Mixfile do
       {:ex_doc, "~> 0.21", only: :dev},
       {:floki, "~> 0.29"},
       {:fast_html, "~> 2.0"},
-      {:poison, "~> 4.0"}
+      {:poison, "~> 4.0 or ~> 5.0"}
     ]
   end
 end
