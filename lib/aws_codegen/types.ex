79af0c2ac1aad2408d62b15c554e9dd7e1b99da7
Onno Vos <onno-vos-dev@users.noreply.github.com>
2024-03-14 17:03:08 +0100

    Shorthand shape_to_type/3

--- lib/aws_codegen/types.ex
+++ lib/aws_codegen/types.ex
@@ -2,123 +2,40 @@ defmodule AWS.CodeGen.Types do
 
   # Unfortunately, gotta patch over auto-defining types that already exist in Elixir
 
-  def shape_to_type(:elixir, "String", _) do
-    "String.t()"
-  end
-  def shape_to_type(:erlang, "String", _) do
-    "string()"
-  end
-
-  def shape_to_type(:elixir, "string", _) do
-    "String.t()"
-  end
-  def shape_to_type(:erlang, "string", _) do
-    "string()"
-  end
-
-  def shape_to_type(:elixir, "Identifier", _) do
-    "String.t()"
-  end
-  def shape_to_type(:erlang, "Identifier", _) do
-    "string()"
-  end
-
-  def shape_to_type(:elixir, "identifier", _) do
-    "String.t()"
-  end
-  def shape_to_type(:erlang, "identifier", _) do
-    "string()"
-  end
-
-  def shape_to_type(:elixir, "XmlString" <> _rest, _) do
-    "String.t()"
-  end
-  def shape_to_type(:erlang, "XmlString" <> _rest, _) do
-    "string"
-  end
-
-  def shape_to_type(:elixir, "NullablePositiveInteger", _) do
-    "nil | non_neg_integer()"
-  end
-  def shape_to_type(:erlang, "NullablePositiveInteger", _) do
-    "undefined | non_neg_integer()"
-  end
-
-  def shape_to_type(_, %AWS.CodeGen.PostService.Shape{type: type}, _module_name) when type in ["float", "double", "long"] do
-    "float()"
-  end
-  def shape_to_type(_, %AWS.CodeGen.RestService.Shape{type: type}, _module_name) when type in ["float", "double", "long"] do
-    "float()"
-  end
-
-  def shape_to_type(_, %AWS.CodeGen.PostService.Shape{type: "timestamp"}, _module_name) do
-    "non_neg_integer()"
-  end
-  def shape_to_type(_, %AWS.CodeGen.RestService.Shape{type: "timestamp"}, _module_name) do
-    "non_neg_integer()"
-  end
-
-  def shape_to_type(_, %AWS.CodeGen.PostService.Shape{type: "map"}, _module_name) do
-    "map()"
-  end
-  def shape_to_type(_, %AWS.CodeGen.RestService.Shape{type: "map"}, _module_name) do
-    "map()"
-  end
-
-  def shape_to_type(_, %AWS.CodeGen.PostService.Shape{type: "blob"}, _module_name) do
-    "binary()"
-  end
-  def shape_to_type(_, %AWS.CodeGen.RestService.Shape{type: "blob"}, _module_name) do
-    "binary()"
-  end
-
-  def shape_to_type(:elixir, %AWS.CodeGen.PostService.Shape{type: "string"}, _module_name) do
-    "String.t()"
-  end
-  def shape_to_type(:erlang, %AWS.CodeGen.PostService.Shape{type: "string"}, _module_name) do
-    "string()"
-  end
-  def shape_to_type(:elixir, %AWS.CodeGen.RestService.Shape{type: "string"}, _module_name) do
-    "String.t()"
-  end
-  def shape_to_type(:erlang, %AWS.CodeGen.RestService.Shape{type: "string"}, _module_name) do
-    "string()"
-  end
-
-  def shape_to_type(_, %AWS.CodeGen.PostService.Shape{type: "integer"}, _module_name) do
-    "integer()"
-  end
-  def shape_to_type(_, %AWS.CodeGen.RestService.Shape{type: "integer"}, _module_name) do
-    "integer()"
-  end
-
-  def shape_to_type(_, %AWS.CodeGen.PostService.Shape{type: "boolean"}, _module_name) do
-    "boolean()"
-  end
-  def shape_to_type(_, %AWS.CodeGen.RestService.Shape{type: "boolean"}, _module_name) do
-    "boolean()"
-  end
-
-  def shape_to_type(_, %AWS.CodeGen.PostService.Shape{type: "enum"}, _module_name) do
-    "list(any())"
-  end
-  def shape_to_type(_, %AWS.CodeGen.RestService.Shape{type: "enum"}, _module_name) do
-    "list(any())"
-  end
-
-  def shape_to_type(_, %AWS.CodeGen.PostService.Shape{type: "union"}, _module_name) do
-    "list()"
-  end
-  def shape_to_type(_, %AWS.CodeGen.RestService.Shape{type: "union"}, _module_name) do
-    "list()"
-  end
-
-  def shape_to_type(_, %AWS.CodeGen.PostService.Shape{type: "document"}, _module_name) do
-    "any()"
-  end
-  def shape_to_type(_, %AWS.CodeGen.RestService.Shape{type: "document"}, _module_name) do
-    "any()"
-  end
+  def shape_to_type(:elixir, "String", _), do: "String.t()"
+  def shape_to_type(:erlang, "String", _), do: "string()"
+  def shape_to_type(:elixir, "string", _), do: "String.t()"
+  def shape_to_type(:erlang, "string", _), do: "string()"
+  def shape_to_type(:elixir, "Identifier", _), do: "String.t()"
+  def shape_to_type(:erlang, "Identifier", _), do: "string()"
+  def shape_to_type(:elixir, "identifier", _), do: "String.t()"
+  def shape_to_type(:erlang, "identifier", _), do: "string()"
+  def shape_to_type(:elixir, "XmlString" <> _rest, _), do: "String.t()"
+  def shape_to_type(:erlang, "XmlString" <> _rest, _), do: "string"
+  def shape_to_type(:elixir, "NullablePositiveInteger", _), do: "nil | non_neg_integer()"
+  def shape_to_type(:erlang, "NullablePositiveInteger", _), do: "undefined | non_neg_integer()"
+  def shape_to_type(_, %AWS.CodeGen.PostService.Shape{type: type}, _module_name) when type in ["float", "double", "long"], do: "float()"
+  def shape_to_type(_, %AWS.CodeGen.RestService.Shape{type: type}, _module_name) when type in ["float", "double", "long"], do: "float()"
+  def shape_to_type(_, %AWS.CodeGen.PostService.Shape{type: "timestamp"}, _module_name), do: "non_neg_integer()"
+  def shape_to_type(_, %AWS.CodeGen.RestService.Shape{type: "timestamp"}, _module_name), do: "non_neg_integer()"
+  def shape_to_type(_, %AWS.CodeGen.PostService.Shape{type: "map"}, _module_name), do: "map()"
+  def shape_to_type(_, %AWS.CodeGen.RestService.Shape{type: "map"}, _module_name), do: "map()"
+  def shape_to_type(_, %AWS.CodeGen.PostService.Shape{type: "blob"}, _module_name), do: "binary()"
+  def shape_to_type(_, %AWS.CodeGen.RestService.Shape{type: "blob"}, _module_name), do: "binary()"
+  def shape_to_type(:elixir, %AWS.CodeGen.PostService.Shape{type: "string"}, _module_name), do: "String.t()"
+  def shape_to_type(:erlang, %AWS.CodeGen.PostService.Shape{type: "string"}, _module_name), do: "string()"
+  def shape_to_type(:elixir, %AWS.CodeGen.RestService.Shape{type: "string"}, _module_name), do: "String.t()"
+  def shape_to_type(:erlang, %AWS.CodeGen.RestService.Shape{type: "string"}, _module_name), do: "string()"
+  def shape_to_type(_, %AWS.CodeGen.PostService.Shape{type: "integer"}, _module_name), do: "integer()"
+  def shape_to_type(_, %AWS.CodeGen.RestService.Shape{type: "integer"}, _module_name), do: "integer()"
+  def shape_to_type(_, %AWS.CodeGen.PostService.Shape{type: "boolean"}, _module_name), do: "boolean()"
+  def shape_to_type(_, %AWS.CodeGen.RestService.Shape{type: "boolean"}, _module_name), do: "boolean()"
+  def shape_to_type(_, %AWS.CodeGen.PostService.Shape{type: "enum"}, _module_name), do: "list(any())"
+  def shape_to_type(_, %AWS.CodeGen.RestService.Shape{type: "enum"}, _module_name), do: "list(any())"
+  def shape_to_type(_, %AWS.CodeGen.PostService.Shape{type: "union"}, _module_name), do: "list()"
+  def shape_to_type(_, %AWS.CodeGen.RestService.Shape{type: "union"}, _module_name), do: "list()"
+  def shape_to_type(_, %AWS.CodeGen.PostService.Shape{type: "document"}, _module_name), do: "any()"
+  def shape_to_type(_, %AWS.CodeGen.RestService.Shape{type: "document"}, _module_name), do: "any()"
 
   def shape_to_type(context, shape_name, module_name, all_shapes) do
     case shape_name do
