7bc5fcd0f3638512341116198f798e5c2a7231e0
Onno Vos <onno-vos-dev@users.noreply.github.com>
2026-05-31 09:49:45 +0100

    Fix test

--- test/aws_codegen/rest_service_test.exs
+++ test/aws_codegen/rest_service_test.exs
@@ -51,7 +51,7 @@ defmodule AWS.CodeGen.RestServiceTest do
                module_name: "AWS.CloudTrailData",
                protocol: "rest-json",
                service_id: "CloudTrail Data",
-               endpoint_url_env_var: "AWS_ENDPOINT_URL_CLOUDTRAIL_DATA",
+               endpoint_url_env_var: "AWS_ENDPOINT_URL_AWS.CLOUDTRAILDATA", # For Erlang this would be: AWS_ENDPOINT_URL_CLOUDTRAILDATA
                signature_version: "v4",
                signing_name: "cloudtrail-data",
                target_prefix: nil
